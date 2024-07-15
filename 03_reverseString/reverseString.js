a19ad1687f385325f1d6d1586093cc799a21ecf7
Camille <camille.ragland@gmail.com>
2024-07-15 17:33:29 -0400

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) 
+{
+    let reverseWord = [];
+    for (let i = word.length; i >= 0; i--) {
+        reverseWord.push(word.charAt(i));
+    }
+    reverseWord =reverseWord.join("");
+    return reverseWord;
 };
 
 // Do not edit below this line
