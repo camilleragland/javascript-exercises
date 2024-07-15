b15d13dfce8af9eb996f010eb2be8053070d3eaf
Camille <camille.ragland@gmail.com>
2024-07-15 10:16:50 -0400

    Finished exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(word,num)
+{
+    let times = 0;
+    phrase = "";
+    if (num >= 0)
+    {
+        while (times < num)
+            {
+                phrase += word;
+                times ++;
+            }
+        return phrase;
+    }
+    else
+    {
+        return "ERROR";
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
